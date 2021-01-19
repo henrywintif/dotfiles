f865c2b1199fc6d4319d9d0615eaeca9f3b7fab8
Henry Winn <henrywinn@henrywinn.com>
2021-01-19 13:06:47 -0500

    Makes open_launcher script into even more generic script

---
+++ .scripts/mutex_run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script opens rofi if it isn't open, and closes it if it is.
+
+my_command=$1
+
+# where the pid_file will go
+pid_file_directory="/tmp/"
+# we'll replace a bunch of possible characters in the command to make the filename safe
+# WARNING: I haven't done extensive testing on this, and I don't guarantee filenames to be safe
+pid_filename=$(echo $my_command | sed -r 's/[][}{|/\!"#$&()*;<=>?^`~\ ]/_/g' | sed -r "s/[']/_/g")
+# the full filepath is the directory+filename
+pid_filepath=$pid_file_directory$pid_filename".lock"
+
+if [ -e $pid_filepath ]
+then
+	# if the pid file exists, my_program is running so we'll kill it
+	pid=`cat $pid_filepath`
+	kill $pid
+	# and remove the file that indicates its running
+	rm $pid_filepath
+else
+	# my_program is not running, so we'll start it
+	eval $my_command" &"
+	pid=$!
+	# save the pid to the file so if this script runs again we can kill it
+	echo $pid >> $pid_filepath
+	# wait until the program closes. this will be triggered both when the
+	# program ends normally (e.g. rofi closes), but also when the program
+	# is killed by another instance of this script, which will actually
+	# run the `rm` command twice, but that shouldn't matter
+	wait $pid
+	rm $pid_filepath
+fi
