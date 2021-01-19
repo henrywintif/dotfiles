7d823299fdfdff8c302e67ef246637a6ecdfe826
Henry Winn <henrywinn@henrywinn.com>
2021-01-19 11:52:20 -0500

    Uses variables to make this script more generic

--- .scripts/open_rofi.sh
+++ .scripts/open_rofi.sh
@@ -2,20 +2,29 @@
 
 # This script opens rofi if it isn't open, and closes it if it is.
 
-my_rofi() {
+my_program() {
 	dmenu_path | rofi -show run &
 }
 
-if [ -e /tmp/rofi_pid ]
+pid_filename="/tmp/rofi_pid"
+
+if [ -e $pid_filename ]
 then
-	# if the rofi_pid file exists, rofi is open so we'll close it
-	rofi_pid=`cat /tmp/rofi_pid`
-	kill $rofi_pid
-	rm /tmp/rofi_pid
+	# if the pid file exists, my_program is running so we'll kill it
+	pid=`cat $pid_filename`
+	kill $pid
+	# and remove the file that indicates its running
+	rm $pid_filename
 else
-	my_rofi
-	rofi_pid=$!
-	echo $rofi_pid >> /tmp/rofi_pid
-	wait $rofi_pid
-	rm /tmp/rofi_pid
+	# my_program is not running, so we'll start it
+	my_program
+	pid=$!
+	# save the pid to the file so if this script runs again we can kill it
+	echo $pid >> $pid_filename
+	# wait until the program closes. this will be triggered both when the
+	# program ends normally (e.g. rofi closes), but also when the program
+	# is killed by another instance of this script, which will actually
+	# run the `rm` command twice, but that shouldn't matter
+	wait $pid
+	rm $pid_filename
 fi
