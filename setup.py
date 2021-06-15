d2e6c1bd6a33d428a0cbd9af96c42b448b4bd8e9
Henry Winn <henrywinn@henrywinn.com>
2021-06-14 21:50:25 -0400

    Fixes bugs in setup script

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import os
 from subprocess import call
 from typing import List
 
+
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description="Used to install or uninstall the dotfiles"
@@ -10,18 +11,18 @@ def init_argparse() -> argparse.ArgumentParser:
     parser.add_argument(
         "command",
         nargs=1,
-        required=True,
         choices=['install', 'i', 'uninstall', 'u']
     )
+    return parser
 
 
 def dependencies_met() -> int:
     deps = [
-        'stowsh'
+        'stow'
     ]
     unmet_deps = []
     for dep in deps:
-        if call(f'which {dep}') > 0:
+        if call(['which', f'{dep}']) > 0:
             unmet_deps.append(dep)
     if len(unmet_deps) > 0:
         print(f'Please install unmet dependencies: {str(unmet_deps)}')
@@ -32,14 +33,15 @@ def dependencies_met() -> int:
 def get_config_dirs() -> List[str]:
     abspath = os.path.abspath(__file__)
     dirname = os.path.dirname(abspath)
-    return [filename for filename in os.listdir(dirname) if os.path.isdir(os.path.join(dirname, filename))]
+    dirs = [filename for filename in os.listdir(dirname)
+            if os.path.isdir(os.path.join(dirname, filename)) and filename[0] != '.']
+    return dirs
 
 
 def install() -> int:
     os.chdir(os.path.expanduser('~'))
     """Change to home dir"""
-    call(['stowsh', '-n'] + get_config_dirs())
-    return 0
+    return call(['stow', '-n'] + get_config_dirs())
 
 
 def uninstall() -> int:
@@ -47,21 +49,22 @@ def uninstall() -> int:
 
 
 def main() -> None:
+    parser = init_argparse()
+    args = parser.parse_args()
+
     dependencies_status = dependencies_met()
     if dependencies_status > 0:
         exit(dependencies_status)
 
-    parser = init_argparse()
-    args = parser.parse_args()
-
-    if args.command[0] == 'i':
+    if args.command[0][0] == 'i':
         exit(install())
 
-    if args.command[0] == 'u':
+    if args.command[0][0] == 'u':
         exit(uninstall())
 
     print('Proper command not given. Run with -h to see help info.')
     exit(0)
+
 
 if __name__ == '__main__':
     main()
